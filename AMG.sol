aa1ab30a52f1a7e843ca3476beb9d81c4500592d
Alex <alext@rtradetechnologies.com>
2019-06-24 13:56:37 -0700

    emit transfer event

--- AMG.sol
+++ AMG.sol
@@ -175,6 +175,7 @@ contract StandardToken is ERC20, BasicToken {
     } else {
       balances[_to] = balances[_to].add(_value);
       allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+      emit Transfer(_from, _to, _value);
     }
     return true;
   }
